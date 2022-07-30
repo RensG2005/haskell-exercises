ba055e94ceb4f349a95d97e48b7c6cc4e290e2c7
RensG2005 <gerritsenREN2@gmail.com>
2022-07-30 14:23:35 +0200

    added examples

--- creditcardValidator.hs
+++ creditcardValidator.hs
@@ -1,17 +1,17 @@
-toDigits :: Integer -> [Integer]
+-- Example: validate 4012888888881881 = True
+-- Example: validate 4012888888881882 = False
 
+toDigits :: Integer -> [Integer]
 toDigits n
     | (n < 1) = []
     | otherwise = (map (\x -> read [x] :: Integer) (show n))
 
 toDigitsRev :: Integer -> [Integer]
-
 toDigitsRev n
     | (n < 1) = []
     | otherwise = reverse (map (\x -> read [x] :: Integer) (show n))
 
 doubleEveryOther :: [Integer] -> [Integer]
-
 doubleEveryOther = reverse . zipWith (*) value . reverse
     where
     value = 1 : 2 : value
